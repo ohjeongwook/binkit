a3c912c8f7eeeac79fb5678100c02aeb33f78352
Matt Oh <oh.jeongwook@gmail.com>
2020-05-22 17:55:51 -0700

    Removed unused table statements

--- SQLiteStorage.h
+++ SQLiteStorage.h
@@ -68,27 +68,6 @@ typedef unsigned char *PBYTE;
 #define UPDATE_BASIC_BLOCK_TABLE_DISASM_LINES_STATEMENT "UPDATE " BASIC_BLOCK_TABLE" SET DisasmLines=%Q WHERE StartAddress='%u';"
 #define UPDATE_BASIC_BLOCK_TABLE_FINGERPRINT_STATEMENT "UPDATE " BASIC_BLOCK_TABLE" SET Fingerprint=%Q WHERE StartAddress='%u';"
 
-#define MATCH_MAP_TABLE "MatchMap"
-#define CREATE_MATCH_MAP_TABLE_STATEMENT "CREATE TABLE " MATCH_MAP_TABLE" ( \n\
-            id INTEGER PRIMARY KEY AUTOINCREMENT, \n\
-            TheSourceFileID INTEGER, \n\
-            TheTargetFileID INTEGER, \n\
-            SourceAddress INTEGER, \n\
-            TargetAddress INTEGER, \n\
-            MatchType INTEGER, \n\
-            Type INTEGER, \n\
-            SubType INTEGER, \n\
-            Status INTEGER, \n\
-            MatchRate INTEGER, \n\
-            UnpatchedParentAddress INTEGER, \n\
-            PatchedParentAddress INTEGER\n\
-         );"
-
-#define INSERT_MATCH_MAP_TABLE_STATEMENT "INSERT INTO  "MATCH_MAP_TABLE" ( TheSourceFileID, TheTargetFileID, SourceAddress, TargetAddress, MatchType, Type, SubType, Status, MatchRate, UnpatchedParentAddress, PatchedParentAddress ) values ( '%u', '%u', '%u', '%u', '%u', '%u', '%u', '%u', '%u', '%u', '%u' );"
-#define DELETE_MATCH_MAP_TABLE_STATEMENT "DELETE FROM "MATCH_MAP_TABLE" WHERE TheSourceFileID=%u and TheTargetFileID=%u"
-#define CREATE_MATCH_MAP_TABLE_SOURCE_ADDRESS_INDEX_STATEMENT "CREATE INDEX "MATCH_MAP_TABLE"SourceAddressIndex ON "MATCH_MAP_TABLE" ( SourceAddress )"
-#define CREATE_MATCH_MAP_TABLE_TARGET_ADDRESS_INDEX_STATEMENT "CREATE INDEX "MATCH_MAP_TABLE"TargetAddressIndex ON "MATCH_MAP_TABLE" ( TargetAddress )"
-
 #define CREATE_FILE_LIST_TABLE_STATEMENT "CREATE TABLE " FILE_LIST_TABLE " ( \n\
             id INTEGER PRIMARY KEY AUTOINCREMENT, \n\
             Type VARCHAR(25), \n\
@@ -108,30 +87,6 @@ typedef unsigned char *PBYTE;
             Address INTEGER\n\
          );"
 #define INSERT_UNIDENTIFIED_BLOCKS_TABLE_STATEMENT "INSERT INTO  "UNIDENTIFIED_BLOCKS_TABLE" ( Type, Address ) values ( '%u', '%u' );"
-
-#define CREATE_FUNCTION_MATCH_INFO_TABLE_STATEMENT "CREATE TABLE " FUNCTION_MATCH_INFO_TABLE" ( \n\
-            id INTEGER PRIMARY KEY AUTOINCREMENT, \n\
-            TheSourceFileID INTEGER, \n\
-            TheTargetFileID INTEGER, \n\
-            SourceAddress INTEGER, \n\
-            EndAddress INTEGER, \n\
-            TargetAddress INTEGER, \n\
-            BlockType INTEGER, \n\
-            MatchRate INTEGER, \n\
-            SourceFunctionName TEXT, \n\
-            Type INTEGER, \n\
-            TargetFunctionName TEXT, \n\
-            MatchCountForTheSource INTEGER, \n\
-            NoneMatchCountForTheSource INTEGER, \n\
-            MatchCountWithModificationForTheSource INTEGER, \n\
-            MatchCountForTheTarget INTEGER, \n\
-            NoneMatchCountForTheTarget INTEGER, \n\
-            MatchCountWithModificationForTheTarget INTEGER, \n\
-            SecurityImplicationsScore INTEGER \n\
-         );"
-#define INSERT_FUNCTION_MATCH_INFO_TABLE_STATEMENT "INSERT INTO  " FUNCTION_MATCH_INFO_TABLE" ( TheSourceFileID, TheTargetFileID, SourceAddress, EndAddress, TargetAddress, BlockType, MatchRate, SourceFunctionName, Type, TargetFunctionName, MatchCountForTheSource, NoneMatchCountForTheSource, MatchCountWithModificationForTheSource, MatchCountForTheTarget, NoneMatchCountForTheTarget, MatchCountWithModificationForTheTarget ) values ( '%u', '%u', '%u', '%u', '%u', '%u', '%u', '%s', '%u', '%s', '%u', '%u', '%u', '%u', '%u', '%u' );"
-#define DELETE_FUNCTION_MATCH_INFO_TABLE_STATEMENT "DELETE FROM "FUNCTION_MATCH_INFO_TABLE" WHERE TheSourceFileID=%u and TheTargetFileID=%u"
-#define CREATE_FUNCTION_MATCH_INFO_TABLE_INDEX_STATEMENT "CREATE INDEX "FUNCTION_MATCH_INFO_TABLE"Index ON "FUNCTION_MATCH_INFO_TABLE" ( TheSourceFileID, TheTargetFileID, SourceAddress, TargetAddress )"
 
 class SQLiteStorage : public Storage
 {
