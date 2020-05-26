f7490d56fed2a6bd00f94ee288b3473de0d92c13
Matt Oh <oh.jeongwook@gmail.com>
2020-05-25 17:33:38 -0700

    Added pybinkit wrapper around Loader

---
+++ pybinkit/module.cpp
@@ -0,0 +1,20 @@
+#include <Windows.h>
+#include <cmath>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "Binary.h"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(pybinkit, m) {
+    py::class_<Binary>(m, "Binary")
+        .def(py::init())
+        .def("open", &Binary::Open)
+        .def("load_basic_blocks", &Binary::LoadBasicBlocks);
+
+    py::class_<BasicBlocks>(m, "BasicBlocks")
+        .def(py::init())
+        .def("get_addresses", &BasicBlocks::GetAddresses)
+        .def("get_symbol", &BasicBlocks::GetSymbol);
+}
