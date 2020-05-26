c0df3edc77fef0c31306ee1fa8eb31f6df2a8a48
Matt Oh <oh.jeongwook@gmail.com>
2020-05-25 20:50:07 -0700

    Added Functions class

--- pybinkit/module.cpp
+++ pybinkit/module.cpp
@@ -11,7 +11,8 @@ PYBIND11_MODULE(pybinkit, m) {
     py::class_<Binary>(m, "Binary")
         .def(py::init())
         .def("open", &Binary::Open)
-        .def("load_basic_blocks", &Binary::LoadBasicBlocks);
+        .def("get_basic_blocks", &Binary::GetBasicBlocks)
+        .def("get_functions", &Binary::GetFunctions);
 
     py::class_<BasicBlocks>(m, "BasicBlocks")
         .def(py::init())
@@ -20,6 +21,9 @@ PYBIND11_MODULE(pybinkit, m) {
         .def("get_instruction_hash", &BasicBlocks::GetInstructionHash)
         .def("get_code_references", &BasicBlocks::GetCodeReferences)
         .def("get_parents", &BasicBlocks::GetParents)
-        .def("get_call_targets", &BasicBlocks::GetCallTargets)        
-        ;        
+        .def("get_call_targets", &BasicBlocks::GetCallTargets);
+
+    py::class_<Functions>(m, "Functions")
+        .def(py::init())
+        .def("get_addresses", &Functions::GetAddresses);
 }
