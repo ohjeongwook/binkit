ff83fb620c37f5e5a1aa0a9cf4829d4626838483
Matt Oh <oh.jeongwook@gmail.com>
2020-05-26 18:15:54 -0700

    Added DiffAlgorithms::DoControlFlowMatch

--- Loader/DiffAlgorithms.h
+++ Loader/DiffAlgorithms.h
@@ -4,6 +4,13 @@
 
 class DiffAlgorithms
 {
+private:
+	BasicBlocks m_srcBasicBlocks;
+	BasicBlocks m_targetBasicBlocks;
 public:
-    vector<MatchData> DoInstructionHashMatch(BasicBlocks& srcBasicBlocks, BasicBlocks& targetBasicBlocks);
+	DiffAlgorithms();
+	DiffAlgorithms(BasicBlocks& srcBasicBlocks, BasicBlocks& targetBasicBlocks);
+    vector<MatchData> DoInstructionHashMatch();
+	int GetInstructionHashMatchRate(vector<unsigned char> instructionHash1, vector<unsigned char> instructionHash2);
+	vector<MatchData> DoControlFlowMatch(va_t sourceAddress, va_t targetAddressess, int type);
 };
