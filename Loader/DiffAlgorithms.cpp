798ca5f6364a70ec2941923b5aa98c61140ea1fd
Matt Oh <oh.jeongwook@gmail.com>
2020-05-26 18:58:52 -0700

    Removed debugging prints

--- Loader/DiffAlgorithms.cpp
+++ Loader/DiffAlgorithms.cpp
@@ -21,7 +21,6 @@ vector<MatchData> DiffAlgorithms::DoInstructionHashMatch()
 	InstructionHashMap *p_srcInstructionHashMap = m_srcBasicBlocks.GetInstructionHashes();
 	InstructionHashMap* p_targetInstructionHashMap = m_targetBasicBlocks.GetInstructionHashes();
 
-	printf("DoInstructionHashMatch: %d vs %d\n", p_srcInstructionHashMap->size(), p_targetInstructionHashMap->size());
 	for (auto& val : *p_srcInstructionHashMap)
 	{
 		// Only when the hash is unique
@@ -37,7 +36,7 @@ vector<MatchData> DiffAlgorithms::DoInstructionHashMatch()
 				matchData.Target = patchedInstructionHashIt->second;
 				matchData.MatchRate = 100;
 
-				LogMessage(0, __FUNCTION__, "%X-%X: %d%%\n", matchData.Source, matchData.Target, matchData.MatchRate);
+				//LogMessage(0, __FUNCTION__, "%X-%X: %d%%\n", matchData.Source, matchData.Target, matchData.MatchRate);
 				matchDataList.push_back(matchData);
 			}
 		}
@@ -48,18 +47,18 @@ vector<MatchData> DiffAlgorithms::DoInstructionHashMatch()
 
 int DiffAlgorithms::GetInstructionHashMatchRate(vector<unsigned char> instructionHash1, vector<unsigned char> instructionHash2)
 {
-	int rate = 0;
+	int matchRate = 0;
 
 	int lengthDifference = (instructionHash1.size() - instructionHash2.size());
 	if (lengthDifference > instructionHash1.size() * 0.5 || lengthDifference > instructionHash2.size() * 0.5)
 	{
-		rate = 0;
+		matchRate = 0;
 	}
 	else
 	{
-		rate = GetStringSimilarity(BytesToHexString(instructionHash1).c_str(), BytesToHexString(instructionHash2).c_str());
+		matchRate = GetStringSimilarity(BytesToHexString(instructionHash1).c_str(), BytesToHexString(instructionHash2).c_str());
 	}
-	return rate;
+	return matchRate;
 }
 
 vector<MatchData> DiffAlgorithms::DoControlFlowMatch(va_t sourceAddress, va_t targetAddressess, int type)
@@ -69,7 +68,6 @@ vector<MatchData> DiffAlgorithms::DoControlFlowMatch(va_t sourceAddress, va_t ta
 
 	vector<va_t> sourceAddresses = m_srcBasicBlocks.GetCodeReferences(sourceAddress, type);
 	vector<va_t> targetAddresses = m_targetBasicBlocks.GetCodeReferences(targetAddressess, type);
-
 
 	if (sourceAddresses.size() == 0 || targetAddresses.size() == 0)
 	{
@@ -114,7 +112,7 @@ vector<MatchData> DiffAlgorithms::DoControlFlowMatch(va_t sourceAddress, va_t ta
 				continue;
 
 			addressPairMap.insert(pair<va_t, va_t>(sourceAddresses[i], targetAddresses[j]));
-			vector<unsigned char> targetInstructionHash = m_targetBasicBlocks.GetInstructionHash(targetAddresses[i]);
+			vector<unsigned char> targetInstructionHash = m_targetBasicBlocks.GetInstructionHash(targetAddresses[j]);
 
 			if (srcInstructionHash.size() > 0 && targetInstructionHash.size() > 0)
 			{
