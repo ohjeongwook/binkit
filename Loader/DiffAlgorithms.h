a64daf043c16f1d745082cc342ec553a215dc9f4
Matt Oh <oh.jeongwook@gmail.com>
2020-05-28 10:42:47 -0700

    Added DoControlFlowMatches algorithm

--- Loader/DiffAlgorithms.h
+++ Loader/DiffAlgorithms.h
@@ -13,4 +13,5 @@ public:
     vector<MatchData> DoInstructionHashMatch();
 	int GetInstructionHashMatchRate(vector<unsigned char> instructionHash1, vector<unsigned char> instructionHash2);
 	vector<MatchData> DoControlFlowMatch(va_t sourceAddress, va_t targetAddressess, int type);
+	vector<MatchData> DoControlFlowMatches(vector<MatchData> inputMatches);
 };
