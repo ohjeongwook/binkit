ef7633e6136efb81c9ef71d99d47f2eb438d6d06
Matt Oh <oh.jeongwook@gmail.com>
2020-06-05 21:09:55 -0700

    Use binary for DiffAlgorithms Test function_matches.do_instruction_hash_match

--- tests/tests.py
+++ tests/tests.py
@@ -60,7 +60,7 @@ class Tests:
         basic_block1 = self.binaries[1].get_basic_blocks()
 
         print('Loading DiffAlgorithms...')
-        diff_algorithms = pybinkit.DiffAlgorithms(basic_block0, basic_block1)
+        diff_algorithms = pybinkit.DiffAlgorithms(self.binaries[0], self.binaries[1])
         print('Performing instruction hash matches...')
         matches = diff_algorithms.do_instruction_hash_match()
         for match in matches:
@@ -90,7 +90,7 @@ class Tests:
         source_basic_block_addresses = src_functions.get_basic_blocks(src_function_address)
         target_basic_block_addresses = target_functions.get_basic_blocks(target_function_address)
 
-        diff_algorithms = pybinkit.DiffAlgorithms(self.binaries[0].get_basic_blocks(), self.binaries[1].get_basic_blocks())
+        diff_algorithms = pybinkit.DiffAlgorithms(self.binaries[0], self.binaries[1])
         for match_data in diff_algorithms.do_instruction_hash_match_in_blocks(source_basic_block_addresses, target_basic_block_addresses):
             print('\t%x - %x : %d%%' % (match_data.source, match_data.target, match_data.match_rate))
 
@@ -105,7 +105,7 @@ class Tests:
             self.print_match_data_combination(match_data_combination, prefix + '\t')
 
     def perform_multilevel_control_flow_matches(self, source, target):
-        diff_algorithms = pybinkit.DiffAlgorithms(self.binaries[0].get_basic_blocks(), self.binaries[1].get_basic_blocks())
+        diff_algorithms = pybinkit.DiffAlgorithms(self.binaries[0], self.binaries[1])
         print('Control Flow Match: %x - %x' % (source, target))
         address_pair = pybinkit.AddressPair(source, target)
         match_data_combinations = diff_algorithms.do_control_flow_matches((address_pair,), CREF_FROM)
@@ -126,13 +126,26 @@ class Tests:
         diff_algorithms = pybinkit.DiffAlgorithms(self.binaries[0], self.binaries[1])
 
         print('Performing instruction hash matches...')
-        matches = diff_algorithms.do_instruction_hash_match()        
-        for function_match in diff_algorithms.do_function_match(matches):
+        matches = diff_algorithms.do_instruction_hash_match()
+
+        function_matches = pybinkit.FunctionMatches(self.binaries[0], self.binaries[1])
+        function_matches.add_matches(matches)
+
+        for function_match in function_matches.get_matches():
             print('%x - %x (size: %d)' % (function_match.source, function_match.target, len(function_match.match_data_list)))
 
-            if function_match.source == 0x6c813b40:
-                match_data_combinations = diff_algorithms.get_match_data_combinations(function_match.match_data_list)
-                self.print_match_data_combinations(match_data_combinations, '\t')
+            match_data_combinations = diff_algorithms.get_match_data_combinations(function_match.match_data_list)
+            self.print_match_data_combinations(match_data_combinations, '\t')
+
+        print('')
+        print('='*80)
+        function_matches.do_instruction_hash_match()
+
+        for function_match in function_matches.get_matches():
+            print('%x - %x (size: %d)' % (function_match.source, function_match.target, len(function_match.match_data_list)))
+
+            #match_data_combinations = diff_algorithms.get_match_data_combinations(function_match.match_data_list)
+            #self.print_match_data_combinations(match_data_combinations, '\t')
 
 if __name__ == '__main__':
     filenames = [r'examples\EPSIMP32-2006.1200.4518.1014.db', r'examples\EPSIMP32-2006.1200.6731.5000.db']
