ebe218e35810dfdbb05751d47417b6c4a4918e67
Matt Oh <oh.jeongwook@gmail.com>
2020-07-11 12:16:24 -0700

    Added install_binkit_plugin.py script

---
+++ Scripts/install_binkit_plugin.py
@@ -0,0 +1,12 @@
+import os
+import urllib2
+plugins_folder = os.path.join(idaapi.get_user_idadir(), "plugins")
+if not os.path.isdir(plugins_folder):
+    os.makedirs(plugins_folder)
+for filename in ('BinKit.dll', 'BinKit64.dll'):
+   url = 'https://github.com/ohjeongwook/binkit/releases/download/v0.1/' + filename
+   local_filename = os.path.join(plugins_folder, filename)
+   print('Downloading %s -> %s' % (url, local_filename))
+   response = urllib2.urlopen(url)
+   with open(os.path.join(plugins_folder, filename), 'wb') as fd:
+      fd.write(response.read())
