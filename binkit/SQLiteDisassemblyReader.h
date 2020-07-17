22542b5bb0b9758ede363bacd9cf37066e5f5226
Matt Oh <oh.jeongwook@gmail.com>
2020-07-17 12:27:39 -0700

    Renamed BasicBlock table name to BasicBlocks

--- binkit/SQLiteDisassemblyReader.h
+++ binkit/SQLiteDisassemblyReader.h
@@ -11,8 +11,8 @@ using namespace std;
 #define FILE_INFO_TABLE "FileInfo"
 #define MAP_INFO_TABLE "ControlFlow"
 #define CREATE_MAP_INFO_TABLE_SRCBLOCK_INDEX_STATEMENT "CREATE INDEX "MAP_INFO_TABLE"Index ON "MAP_INFO_TABLE" (SrcBlock)"
-#define BASIC_BLOCK_TABLE "BasicBlock"
-#define UPDATE_BASIC_BLOCK_TABLE_FUNCTION_ADDRESS_STATEMENT "UPDATE " BASIC_BLOCK_TABLE" SET FunctionAddress='%u',BlockType='%d' WHERE FileID='%u' AND StartAddress='%u';"
+#define BASIC_BLOCKS_TABLE "BasicBlocks"
+#define UPDATE_BASIC_BLOCKS_TABLE_FUNCTION_ADDRESS_STATEMENT "UPDATE " BASIC_BLOCKS_TABLE" SET FunctionAddress='%u',BlockType='%d' WHERE FileID='%u' AND StartAddress='%u';"
 
 class SQLiteDisassemblyReader : public DisassemblyReader
 {
