dee81466f457f334768631f8d3cd79948dcc83ed
Matt Oh <oh.jeongwook@gmail.com>
2020-07-17 11:49:06 -0700

    Guess plugin location when idaapi is not available

--- scripts/install_binkit_plugin.py
+++ scripts/install_binkit_plugin.py
@@ -1,6 +1,13 @@
 import os
 import urllib2
-plugins_folder = os.path.join(idaapi.get_user_idadir(), "plugins")
+
+try:
+    idadir = idaapi.get_user_idadir()
+except:
+    idadir = os.path.join(os.environ['USERPROFILE'], r'AppData\Roaming\Hex-Rays\IDA Pro')
+
+plugins_folder = os.path.join(idadir, "plugins")
+
 if not os.path.isdir(plugins_folder):
     os.makedirs(plugins_folder)
 for filename in ('binkit.dll', 'binkit64.dll'):
