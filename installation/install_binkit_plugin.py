747b16edd95f6a8cc6ed047e62f12c56bd413e2c
Matt Oh <oh.jeongwook@gmail.com>
2020-08-20 18:19:16 -0700

    Updated installation script

--- installation/install_binkit_plugin.py
+++ installation/install_binkit_plugin.py
@@ -1,5 +1,6 @@
 import os
 import urllib2
+import zipfile
 
 try:
     idadir = idaapi.get_user_idadir()
@@ -10,10 +11,18 @@ plugins_folder = os.path.join(idadir, "plugins")
 
 if not os.path.isdir(plugins_folder):
     os.makedirs(plugins_folder)
-for filename in ('binkit.dll', 'binkit64.dll'):
-   url = 'https://github.com/ohjeongwook/binkit/releases/download/v0.1/' + filename
-   local_filename = os.path.join(plugins_folder, filename)
-   print('Downloading %s -> %s' % (url, local_filename))
-   response = urllib2.urlopen(url)
-   with open(local_filename, 'wb') as fd:
-      fd.write(response.read())
+
+filename = 'plugins.zip'
+url = 'https://github.com/ohjeongwook/binkit/releases/download/v0.2/' + filename
+local_filename = os.path.join(plugins_folder, filename)
+print('Downloading %s -> %s' % (url, local_filename))
+response = urllib2.urlopen(url)
+with open(local_filename, 'wb') as fd:
+    fd.write(response.read())
+
+print('Extracting %s -> %s' % (local_filename, plugins_folder))
+with zipfile.ZipFile(local_filename, 'r') as zip_ref:
+    zip_ref.extractall(plugins_folder)
+
+print('Removing %s' % (local_filename))
+os.remove(local_filename)
