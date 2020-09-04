1f1bc189bd1ad72b19d450f94298f1dab8c203be
Matt Oh <oh.jeongwook@gmail.com>
2020-09-04 12:28:09 -0700

    updated project

--- src/exporter/Log.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <windows.h>
-#include <stdarg.h>
-#include <wtypes.h>
-
-void SetLogLevel(int logLevel);
-void LogMessage(int level, const char *function_name, const TCHAR *format, ...);
